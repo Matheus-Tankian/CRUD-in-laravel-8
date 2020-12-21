c1efddac4006ec3749f617ad39c7bfe0a8820219
Matheus <matheusvitor8143@gmail.com>
2020-12-20 22:47:48 -0300

    segundo

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
   
 @section('content')
-<div class="row">
+<div class="row" style="margin-top:30px">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Add New Product</h2>
+            <h2>Add Novo Produto</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
@@ -14,7 +14,7 @@
    
 @if ($errors->any())
     <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <strong>Whoops!</strong>Desculpa mas nos tivemos um problema com seu input.<br><br>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -29,14 +29,14 @@
      <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Name">
+                <strong>Nome:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Nome">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail"></textarea>
+                <strong>Detalhes:</strong>
+                <textarea class="form-control" style="height:150px" name="detail" placeholder="Detalhes"></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
   
 @section('content')
-    <div class="row">
+    <div class="row" style="margin-top:30px">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Show Product</h2>
+                <h2> Show Produto</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
@@ -15,13 +15,13 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
+                <strong>Nome:</strong>
                 {{ $product->name }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Details:</strong>
+                <strong>Detalhes:</strong>
                 {{ $product->detail }}
             </div>
         </div>
